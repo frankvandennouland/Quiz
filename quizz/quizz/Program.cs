eacfa9412fbfa4e1ecb157e6e5d368285a7996fa
frankvandennouland <frankvandennouland@gmail.com>
2017-11-19 20:56:38 +0100

    Meerkeuze toegevoegd

--- quizz/quizz/Program.cs
+++ quizz/quizz/Program.cs
@@ -17,11 +17,21 @@ namespace quizz
             Question vraag3 = new Question() { Vraag = "test vraag 3 antwoord is 3", Moeilijkheidsgraad = 3, Antwoord = "2", Categorie = "dieren" };
             Question vraag4 = new Question() { Vraag = "test vraag 4 antwoord is 4", Moeilijkheidsgraad = 2, Antwoord = "4", Categorie = "heelal" };
 
+            Question vraag6 = new Question() { Vraag = "meerkeuze vraag", Moeilijkheidsgraad = 2, Antwoord = "3", Categorie = "meerkeuze" };
+            Option option1 = new Option() { Number = 1, Anwser = "dit is het foute antwoord" };
+            Option option2 = new Option() { Number = 2, Anwser = "dit is het bijna goede antwoord" };
+            Option option3 = new Option() { Number = 2, Anwser = "dit is het goede antwoord" };
+
+            vraag6.addOption(option1);
+            vraag6.addOption(option2);
+            vraag6.addOption(option3);
+
             questions.Add(vraag1);
             questions.Add(vraag2);
             questions.Add(vraag3);
             questions.Add(vraag4);
             questions.Add(vraag5);
+            questions.Add(vraag6);
 
 
             Console.WriteLine("Welkom! wilt u de quizz beginnen? (Druk op een toets)");
@@ -41,9 +51,8 @@ namespace quizz
                 int.TryParse(Console.ReadLine(), out moeilijkheidsgraad);
                 Console.WriteLine();
 
-                Console.WriteLine("Wat voor soort categorie wilt u? (algemeen, landen, dieren, heelal)");
+                Console.WriteLine("Wat voor soort categorie wilt u? (algemeen, landen, dieren, heelal, meerkeuze)");
                 var categorie = Console.ReadLine();
-                Console.WriteLine(moeilijkheidsgraad + "  " + categorie);
 
                 NewQuestions = questions.Where(s => s.Moeilijkheidsgraad.Equals(moeilijkheidsgraad) && s.Categorie.Equals(categorie)).ToArray();
             } else
@@ -59,6 +68,14 @@ namespace quizz
             foreach (Question q in Playquestions)
             {
                 Console.WriteLine("Vraag " + teller + ". " + q.Vraag);
+
+                int optieteller = 1;
+                foreach(Option o in q.Options)
+                {
+                    Console.WriteLine("optie " + optieteller + ". " + o.Anwser);
+                    optieteller++;
+                }
+
                 Console.WriteLine("Vul je antwoord in:");
 
                 q.GebruikerAntwoord = Console.ReadLine();
--- quizz/quizz/Question.cs
+++ quizz/quizz/Question.cs
@@ -11,16 +11,23 @@ namespace quizz
         private string categorie;
         private string antwoord;
         private string gebruikerAntwoord;
+        private List<Option> options = new List<Option>();
 
         public int Moeilijkheidsgraad { get { return moeilijkheidsgraad; }  set { moeilijkheidsgraad = value; } }
         public string Vraag { get { return vraag; } set { vraag = value; } }
         public string Categorie { get { return categorie; } set { categorie = value; } }
         public string Antwoord { get { return antwoord; } set { antwoord = value; } }
         public string GebruikerAntwoord { get { return gebruikerAntwoord; } set { gebruikerAntwoord = value; } }
+        public List<Option> Options { get { return options; } }
 
         public Boolean Checkantwoord()
         {
             return (antwoord == gebruikerAntwoord);
+        }
+
+        public void addOption(Option option)
+        {
+            options.Add(option);
         }
     }
 }
